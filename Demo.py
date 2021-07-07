b0082a021d97ccab972e7f9831e3bf02b77bf790
Kulkarni Manali <68278965+ManaliKulkarni30@users.noreply.github.com>
2021-07-07 11:55:02 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,12 @@
+#Complete the code using KNN
+import pandas as pd
+
+def main():
+    df = pd.read_csv('iris_git.csv')
+    #print(df)
+    print(df.head())
+
+if __name__ == '__main__':
+    main()
+
+#iloc
